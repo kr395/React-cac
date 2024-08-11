9e54bc845b3004fceb906857b2017d122bfcb108
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-08-11 12:10:29 +0530

    Tailwind and components learnt

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import Card from "./components/card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-white p-2 mb-1">Tellwind Text</h1>
+      <Card name = "About Ipad"/>
+    </>
+  );
+}
+
+export default App;
