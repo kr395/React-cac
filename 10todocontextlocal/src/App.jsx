4350f109de194eb65c8a7cb4479b9879496069eb
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-08-19 23:28:12 +0530

    todo context API and Local Storage Project started

---
+++ 10todocontextlocal/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-3xl text-center font-mono text-green-400'>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 10todocontextlocal/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext = createContext({
+   todos : [
+   {
+    id: 1,
+    todo : "Drink Water",
+    completed : false,
+
+   }
+   ],
+   adTodo : (todo)=>{},
+   updateTodo : (id,todo)=>{},
+   deleteTodo : (id)=>{},
+   toggleComplete : (id)=>{}
+
+});
+
+export const useTodoContext = ()=>{
+  return useContext(TodoContext);
+}
+
+export const TodoContextProvider = TodoContext.Provider;
