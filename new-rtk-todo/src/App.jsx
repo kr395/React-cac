22808760205137a7690d4f1862780475c69668eb
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-11-26 23:09:37 +0530

    new RTK Added

---
+++ new-rtk-todo/src/App.jsx
@@ -0,0 +1,17 @@
+import { AddTodo } from './Components/AddTodo'
+import Todos from './Components/Todos'
+import './App.css'
+function App() {
+
+  return (
+    <>
+
+    <h1> Hello Guyes</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+
+  )
+}
+
+export default App
---
+++ new-rtk-todo/src/Components/Todos.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            > ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Todos;
